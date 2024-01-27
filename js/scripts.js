f1ecb888e857243f372ceeb47a80d94b888f267c
IlyaMuralha <muralhakuzmenko@gmail.com>
2024-01-27 18:57:04 +0500

    Added services section

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+// Разворачивает меню при нажатии
 $(function () {
     $('.mini-menu').click(function () {
         if ($('.nav-list').css('display') == 'none') {
             $('.nav-list').slideDown()
         } else {
             $('.nav-list').slideUp()
-        }
-        
+        }    
     });
+
+    // Slick слайдер
+
+    $('.service-slider').slick({
+       arrows: false,
+       dots: true,
+       responsive: [{
+        breakpoint: 600,
+        settings: {
+            dots: false,
+            centerMode: true,
+            centerPadding: '50px'
+        }
+       }]
+    })
+
 });
